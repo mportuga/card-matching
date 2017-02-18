5f8724d0924a4f4aebfeb5eb257aa2b5733549b9
Portugal, Marcelo <Marcelo.Portugal@Kronos.com>
2017-02-18 14:23:17 -0500

    chore(angular2): Adding angular2 quickstart seed into the application.
    Modified the angular2 quickstart seed to work with express and updated gitignore file.

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var express = require('express'),
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
-});
+app.use(express.static('dist'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
